627901192c25778b5222395acb09875b93daca08
Ruichao Guan <mikkeguan@gmail.com>
2024-11-05 11:42:30 +0200

    first api call

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, TextInput} from "react-native";
+import { useState, useEffect } from "react";
+import {url} from "../utils/utils";
+ export default function Index() {
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  const [data, setData] = useState();
+  
+  const fetchInfo = () => { 
+    return fetch(url) 
+            .then((res) => res.json()) 
+            .then((d) => setData(d)) 
+    }
+    
+    useEffect(() => {
+      fetchInfo();
+    }, [])
+    return (
+      <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+        {data ? (
+          <>
+          <TextInput placeholder="Enter Pokemon Name"></TextInput>
+
+            <Text>{data.name.toUpperCase()}</Text>
+            <Image
+              source={{ uri: data.sprites.front_default }}
+              style={{ width: 100, height: 100 }}
+            />
+          </>
+        ) : (
+          <Text>Loading...</Text>
+        )}
+      </View>
+    );
 }
