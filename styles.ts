290ed0259187f958ca7784c16491f187398bd103
Ruichao Guan <mikkeguan@gmail.com>
2024-11-28 23:13:51 +0200

    input field styling

--- styles.ts
+++ styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -10,49 +10,59 @@ const styles = StyleSheet.create({
   input: {
     width: 200,
     height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    marginVertical: 10,
+    borderColor:"lightgreen",
+    borderWidth: 3,
+    borderRadius: 8,
+    backgroundColor: "white",
+    paddingHorizontal: 10,
+    color: "darkgreen",
+    marginBottom: 10,
+    shadowColor: "black",
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 2,
+    margin: 10,
   },
+
   pokemonImage: {
     width: 200,
     height: 200,
     borderWidth: 2,
   },
-  pokemonType:{
+  pokemonType: {
     color: "gray",
     fontWeight: "bold",
     fontSize: 16,
-},
+  },
   pokemonName: {
     fontWeight: "bold",
-    fontSize: 16, 
-   },
-    logo: {
-      width: 80,
-      height: 80,
-    },
+    fontSize: 16,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+  },
 
-    loadingContainer: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center", 
-      position: "absolute",
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-    },      
-    header: {
-      fontSize: 40,
-      fontWeight: "bold",
-      marginBottom: 10,
-      color: "#ffffff", 
-      textShadowColor: "rgba(0, 0, 0, 0.75)",
-      textShadowOffset: { width: -1, height: 1 },
-      textShadowRadius: 10,
-    },
-  
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: "bold",
+    marginBottom: 10,
+    color: "#ffffff",
+    textShadowColor: "rgba(0, 0, 0, 0.75)",
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
+  },
 });
 
 export default styles;
